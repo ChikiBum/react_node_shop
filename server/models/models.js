6d9b2524491c6d4b7f1c2b03bdfab2659e0ec65b
Olexander Bozhko <obozhko82@gmail.com>
2022-02-01 12:39:50 +0200

    add routerrs, controllers

--- server/models/models.js
+++ server/models/models.js
@@ -31,7 +31,7 @@ const Type = sequelize.define('type', {
 
 const Brand = sequelize.define('brand', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-    name: {type: DataTypes.INTEGER, allowNull: false},
+    name: {type: DataTypes.STRING, unique: true, allowNull: false},
 });
 
 const Rating = sequelize.define('rating', {
